ceafcc3448913882a9625eb0463ccced335328d8
Nini <gamelani@ethz.ch>
2018-06-13 13:58:44 -0700

    Added config file

---
+++ src/config.py
@@ -0,0 +1,14 @@
+""""
+File containing configuration variables used across the project
+"""
+
+# path to data folder and data sets
+data_folder = '../data'
+
+data_label_path = data_folder + "/labeled.csv"
+data_score_path = data_folder + "/scored.csv"
+label_img_folder = data_folder + "/labeled/"
+score_img_folder = data_folder + "/scored/"
+query_img_folder = data_folder + "/query/"
+
+# path to
