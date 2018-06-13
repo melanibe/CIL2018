83e5a2c0ee759659e5f74b29f751d388f6c58536
melanibe <melanibe@student.ethz.ch>
2018-06-13 09:21:30 +0200

    added loading function

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,56 @@
 import numpy as np
 import os
+import pandas as pd
+from skimage import io
+
+global cwd
+cwd = os.getcwd()
+
+data_label_path = cwd + "/data/labeled.csv"
+data_score_path = cwd + "/data/scored.csv"
+label_img_folder = cwd + "/data/labeled/"
+score_img_folder = cwd + "/data/scored/"
+query_img_folder = cwd + "/data/query/"
+
+def load_data(type, path=None, csv_file = None): #type=labeled or scored (query special case without csv later)
+	"""This function creates a dataframe with:
+			- a column containg the image (array)
+			- a column 'labeled' with the label if type = labeled
+			 OR a column 'scored' with the score if type = score
+			 OR no other column if type = query
+			- the row index of the dataframe is the name of the image
+	Examples:
+	>>> df_label = load_data('labeled')
+	>>> df_label.loc[9397734]
+		img        [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,...
+		labeled                                                    1
+		Name: 9397734, dtype: object
+	>>> df_score = load_data('scored')
+	>>> df_score.loc[5694059]
+		img       [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,...
+		scored                                              1.83779
+		Name: 5694059, dtype: object
+	>>> df_query = load_data('query')
+	>>> df_query.loc[1000956]
+		img    [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,...
+		Name: 1000956, dtype: object
+	"""											
+	result = pd.DataFrame()
+	images_array=[]
+	ids = [] #name of the images to retrieve in csv file
+	if path == None:
+		path = cwd + "/data/{}/".format(type)
+	for image in os.listdir(path):
+		images_array.append(io.imread(os.path.join(path, image)))
+		ids.append(int(image.replace(".png","")))
+	result['img'] = pd.Series(images_array, index=ids)	
+	if type != 'query':
+		if csv_file == None:
+			csv_file = cwd + "/data/{}.csv".format(type)
+		#loading the score or the label
+		df = pd.read_csv(csv_file)
+		result[type]= pd.Series(df['Actual'].values, index=df['Id'].values)
+	return result
 
 
 #FUNCTION DEFINED IN SERIES 5 SOLUTION
