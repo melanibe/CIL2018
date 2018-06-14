5be81ad36f3db82277177beb82c35a4f9384c996
melanibe <melanibe@student.ethz.ch>
2018-06-14 12:45:52 +0200

    modified data path was not working before

--- src/config.py
+++ src/config.py
@@ -3,12 +3,17 @@ File containing configuration variables used across the project
 """
 
 # path to data folder and data sets
-data_folder = '../data'
+#RUN IT FROM MAIN CODE FOLDER so no .. !!!!!!
 
-data_label_path = data_folder + "/labeled.csv"
-data_score_path = data_folder + "/scored.csv"
-label_img_folder = data_folder + "/labeled/"
-score_img_folder = data_folder + "/scored/"
-query_img_folder = data_folder + "/query/"
+import os
+global cwd
+cwd = os.getcwd()
+## KEEP CWD !
+data_folder = cwd+'/data/'
 
-# path to
+data_label_path = data_folder + "labeled.csv"
+data_score_path = data_folder + "scored.csv"
+label_img_folder = data_folder + "labeled/"
+score_img_folder = data_folder + "scored/"
+query_img_folder = data_folder + "query/"
+
