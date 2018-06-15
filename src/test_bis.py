ef32d11b5bb84ff9344411bf4b80820b2c95f885
melanibe <melanibe@student.ethz.ch>
2018-06-15 10:51:25 +0200

    everything for "my" model

---
+++ src/test_bis.py
@@ -0,0 +1,82 @@
+import preprocessing
+from model import model_skeleton_bis
+import numpy as np
+import tensorflow as tf
+import os
+
+"""
+To predict
+"""
+cwd = os.getcwd()
+## PARAMETERS ##
+run_number = 1529014670
+# Data loading parameters
+#tf.flags.DEFINE_string("data_file_path", "/data/sentences_test.txt", "Path to the test data. This data should be distinct from the training data.")
+tf.flags.DEFINE_integer("train run number", run_number, "")
+# Test parameters
+tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
+tf.flags.DEFINE_string("checkpoint_dir", "./runs/{}/checkpoints/".format(run_number), "Checkpoint directory from training run") #for cluster
+# Tensorflow Parameters
+tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
+tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+
+FLAGS = tf.flags.FLAGS
+
+print("\nParameters:")
+for attr, value in sorted(FLAGS.__flags.items()):
+	print("{}={}".format(attr.upper(), value.value))
+print("")
+
+## DATA PREPARATION ##
+
+# Load data
+data_query = preprocessing.load_data("query")
+imgs = np.reshape(np.array(data_query['img'].values), (-1,1))
+id = np.reshape(np.array(data_query.index.values), (-1,1))
+query_data = np.concatenate((imgs,id), axis = 1)
+
+## EVALUATION ##
+#checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+checkpoint_file = cwd+"/runs/{}/checkpoints/model-44500".format(run_number) #for running test locally with model from cluster
+graph = tf.Graph()
+with graph.as_default():
+	session_conf = tf.ConfigProto(
+		allow_soft_placement=FLAGS.allow_soft_placement,
+		log_device_placement=FLAGS.log_device_placement)
+	sess = tf.Session(config=session_conf)
+	with sess.as_default():
+
+		# Load the saved meta graph and restore variables
+		saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
+		saver.restore(sess, checkpoint_file)
+
+		# Get the placeholders from the graph by name
+		input = graph.get_operation_by_name("input_img").outputs[0]
+
+		# Tensors we want to evaluate
+		scores = graph.get_operation_by_name("output/Reshape").outputs[0] #this has to be changed to output/score_pred with the new version of model
+
+		# Generate batches for one epoch
+		batches = preprocessing.batch_iter(query_data, FLAGS.batch_size, 1, shuffle=False)
+
+		timestamp = str(run_number)
+		out_dir = os.path.abspath(os.path.join(os.path.curdir, "predictions"))
+		
+		# Create the directory perplexities
+		if not os.path.exists(out_dir):
+			os.makedirs(out_dir)
+
+		#out_dir = os.path.abspath(os.path.join(out_dir, timestamp))
+		#for final submission
+		out_file = os.path.abspath(os.path.join(out_dir, "{}".format(timestamp)))
+		print("Writing to {}\n".format(out_file))
+
+		with open("{}.csv".format(out_dir),"w") as file:
+			file.write('Id,Predicted\n') #create the header
+			for test_batch in batches: # 
+				batch_imgs = np.reshape(np.concatenate(test_batch[:, 0]), (-1,1000,1000))
+				batch_id = np.reshape(test_batch[:, 1], (-1))
+				batch_scores = sess.run(scores, {input: batch_imgs})
+				for i in range(len(batch_scores)):
+					# Write perplexity in ./perplexities/
+					file.write('{},{}\n'.format(batch_id[i], batch_scores[i])) #csv id, score
---
+++ src/train_bis.py
@@ -0,0 +1,282 @@
+import preprocessing
+from model.model_skeleton_bis import Discriminator
+from config import *
+import pandas as pd
+
+import tensorflow as tf
+import numpy as np
+import os
+import time
+import datetime
+
+import sys
+
+import pickle
+
+"""Launch file for training tasks
+"""
+
+## PARAMETERS ##
+
+# Data loading parameters
+tf.flags.DEFINE_float("dev_sample_percentage", .01, "Percentage of the training data used for validation")
+
+# Model parameters
+tf.flags.DEFINE_boolean("reuse", False, "reuse")
+tf.flags.DEFINE_string("discr_type", "regressor", "type of discriminant")
+tf.flags.DEFINE_integer("filter_height", 10, "filter_height")
+tf.flags.DEFINE_integer("filter_width", 10, "filter_width")
+tf.flags.DEFINE_integer("out_channels1", 64, "out_channels1")
+
+# Training parameters
+tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
+tf.flags.DEFINE_integer("num_epochs", 60, "Number of training epochs (default: 200)")
+tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
+tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
+tf.flags.DEFINE_integer("num_checkpoints", 2, "Number of checkpoints to store (default: 5)")
+
+# Tensorflow Parameters
+tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
+tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+
+# for running on EULER
+tf.flags.DEFINE_integer("inter_op_parallelism_threads", 16,
+	"TF nodes that perform blocking operations are enqueued on a pool of inter_op_parallelism_threads available in each process (default 0).")
+tf.flags.DEFINE_integer("intra_op_parallelism_threads", 16,
+	"The execution of an individual op (for some op types) can be parallelized on a pool of intra_op_parallelism_threads (default: 0).")
+
+FLAGS = tf.flags.FLAGS
+
+print("\nParameters:")
+for attr, value in sorted(FLAGS.__flags.items()):
+	print("{}={}".format(attr.upper(), value.value))
+print("")
+
+
+###############################  DATA PREPARATION   ###############################
+# Load data
+print("Loading and preprocessing training... \n")
+
+# Saving or loading the objects:
+""" too big for cluster
+if os.path.isfile("score.pickle"):
+	max_bytes = 2**31 - 1
+	input_size = os.path.getsize("score.pickle")
+	bytes_in = bytearray(0)
+	with open("score.pickle", 'rb') as f_in:
+		for _ in range(0, input_size, max_bytes):
+			bytes_in += f_in.read(max_bytes)
+	scored_data = pickle.loads(bytes_in)
+
+else:
+	data_scored = preprocessing.load_data("scored")
+	imgs = np.reshape(np.array(data_scored['img'].values), (-1,1)) #dim: 9600*1000*1000
+	scores = np.reshape(np.array(data_scored['scored'].values), (-1,1)) #dim: 9600
+	scored_data = np.concatenate((imgs,scores), axis = 1)
+	max_bytes = 2**31 - 1
+	bytes_out = pickle.dumps(scored_data)
+	n_bytes = sys.getsizeof(bytes_out)
+	try:
+		with open("score.pickle", 'wb') as f_out:
+			for idx in range(0, n_bytes, max_bytes):
+				f_out.write(bytes_out[idx:idx+max_bytes])
+	except:
+		pass
+
+##### same for labell
+if os.path.isfile("label.pickle"):
+	max_bytes = 2**31 - 1
+	input_size = os.path.getsize("label.pickle")
+	bytes_in = bytearray(0)
+	with open("label.pickle", 'rb') as f_in:
+		for _ in range(0, input_size, max_bytes):
+			bytes_in += f_in.read(max_bytes)
+	labeled_data = pickle.loads(bytes_in)
+
+else:
+	labeled_data = preprocessing.load_data("labeled")
+	imgs = np.reshape(np.array(labeled_data['img'].values), (-1,1)) #dim: 9600*1000*1000
+	labels = np.reshape(np.array(labeled_data['labeled'].values), (-1,1)) #dim: 9600
+	labeled_data = np.concatenate((imgs,labels), axis = 1)
+	max_bytes = 2**31 - 1
+	bytes_out = pickle.dumps(labeled_data)
+	n_bytes = sys.getsizeof(bytes_out)
+	try:
+		with open("label.pickle", 'wb') as f_out:
+			for idx in range(0, n_bytes, max_bytes):
+				f_out.write(bytes_out[idx:idx+max_bytes])
+	except:
+		pass
+
+"""
+data_scored = preprocessing.load_data("scored")
+imgs = np.reshape(np.array(data_scored['img'].values), (-1,1)) #dim: 9600*1000*1000
+scores = np.reshape(np.array(data_scored['scored'].values), (-1,1)) #dim: 9600
+scored_data = np.concatenate((imgs,scores), axis = 1)
+labeled_data = preprocessing.load_data("labeled")
+imgs = np.reshape(np.array(labeled_data['img'].values), (-1,1)) #dim: 9600*1000*1000
+labels = np.reshape(np.array(labeled_data['labeled'].values), (-1,1)) #dim: 9600
+labeled_data = np.concatenate((imgs,labels), axis = 1)
+
+
+print("Data loaded")
+# Randomly shuffle data
+np.random.seed(10)
+
+#train/test split for score
+shuffled_indices = np.random.permutation(len(scored_data[:,0]))
+dev_sample_index = int(FLAGS.dev_sample_percentage * float(len(shuffled_indices)))
+test_indices = shuffled_indices[:dev_sample_index]
+train_indices = shuffled_indices[dev_sample_index:]
+train_score = scored_data[train_indices,:]
+test_scored = scored_data[test_indices,:]
+#needed for dev step
+test_score_imgs = np.reshape(np.concatenate(test_scored[:, 0]), (-1,1000,1000))
+test_score = np.reshape(test_scored[:, 1], (-1))
+
+
+#train/test split for labels
+shuffled_indices = np.random.permutation(len(labeled_data))
+dev_sample_index = int(FLAGS.dev_sample_percentage * float(len(shuffled_indices)))
+test_indices = shuffled_indices[:dev_sample_index]
+train_indices = shuffled_indices[dev_sample_index:]
+train_label = labeled_data[train_indices,:]
+test_labeled = labeled_data[test_indices,:]
+#needed for dev step
+test_label_imgs = np.reshape(np.concatenate(test_labeled[:, 0]), (-1,1000,1000))
+test_label = np.reshape(test_labeled[:, 1], (-1))
+
+# Generate training batches 
+batches_label = preprocessing.batch_iter(train_label, FLAGS.batch_size, FLAGS.num_epochs) 
+batches_score = preprocessing.batch_iter(train_score, FLAGS.batch_size, FLAGS.num_epochs)
+
+
+## MODEL AND TRAINING PROCEDURE DEFINITION ##
+
+graph = tf.Graph()
+with graph.as_default():
+	session_conf = tf.ConfigProto(
+		allow_soft_placement=FLAGS.allow_soft_placement,
+		log_device_placement=FLAGS.log_device_placement,
+		inter_op_parallelism_threads=FLAGS.inter_op_parallelism_threads,
+		intra_op_parallelism_threads=FLAGS.intra_op_parallelism_threads
+		)
+	sess = tf.Session(config=session_conf)
+	with sess.as_default():
+		# Initialize model
+		discr = Discriminator(reuse=FLAGS.reuse, \
+							discr_type=FLAGS.discr_type, \
+							filter_height = FLAGS.filter_height,\
+							 filter_width = FLAGS.filter_width, \
+							 out_channels1 = FLAGS.out_channels1)
+
+		# Define an optimizer with clipping the gradients
+		global_step = tf.Variable(0, name="global_step", trainable= False)
+		optimizer = tf.train.AdamOptimizer()
+		gradient_var_pairs = optimizer.compute_gradients(discr.loss)
+		vars = [x[1] for x in gradient_var_pairs]
+		gradients = [x[0] for x in gradient_var_pairs]
+		clipped, _ = tf.clip_by_global_norm(gradients, 5)
+		train_op = optimizer.apply_gradients(zip(clipped, vars), global_step = global_step)
+		
+		# Output directory for models and summaries
+		timestamp = str(int(time.time()))
+		out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
+		print("Writing to {}\n".format(out_dir))
+
+		# Loss summaries
+		loss_summary = tf.summary.scalar("loss", discr.loss)
+
+		# Train summaries
+		train_summary_op = tf.summary.merge([loss_summary])
+		train_summary_dir = os.path.join(out_dir, "summaries", "train")
+		train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
+
+		# Dev summaries
+		dev_summary_op = tf.summary.merge([loss_summary])
+		dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
+		dev_summary_writer = tf.summary.FileWriter(dev_summary_dir, sess.graph)
+
+		# Checkpoint directory (Tensorflow assumes this directory already exists so we need to create it)
+		checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+		checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+		if not os.path.exists(checkpoint_dir):
+			os.makedirs(checkpoint_dir)
+		saver = tf.train.Saver(tf.global_variables(), max_to_keep=FLAGS.num_checkpoints)
+
+		# Initialize all variables
+		sess.run(tf.global_variables_initializer())
+		sess.graph.finalize()
+
+		# Define training and dev steps (batch)
+		def train_step(batch_imgs, batch_score, type= FLAGS.discr_type):
+			"""
+			A single training step
+			"""
+			if type == "regressor":
+				feed_dict = {
+					discr.input_img: batch_imgs,
+					discr.scores: batch_score
+				}
+			else:
+				feed_dict = {
+					discr.input_img: batch_imgs,
+					discr.labels: batch_score
+				}				
+			_, step, summaries, loss = sess.run(
+				[train_op, global_step, train_summary_op, discr.loss],
+				feed_dict)
+			time_str = datetime.datetime.now().isoformat()
+			print("{}: step {}, loss {:g}".format(time_str, step, loss))
+			train_summary_writer.add_summary(summaries, step)
+
+		def dev_step(batch_imgs, batch_score, type= FLAGS.discr_type, writer=None):
+			"""
+			Evaluates model on a dev set
+			"""
+			if type == "regressor":
+				feed_dict = {
+					discr.input_img: batch_imgs,
+					discr.scores: batch_score
+				}
+			else:
+				feed_dict = {
+					discr.input_img: batch_imgs,
+					discr.labels: batch_score
+				}
+			step, summaries, loss = sess.run(
+				[global_step, dev_summary_op, discr.loss],
+				feed_dict)
+			time_str = datetime.datetime.now().isoformat()
+			print("{}: step {}, loss {:g}".format(time_str, step, loss))
+			if writer:
+				writer.add_summary(summaries, step)
+
+		## TRAINING LOOP ##  
+		#####we only could modify this later to train one discriminator on both
+		if FLAGS.discr_type=="regressor":
+			for batch in batches_score:
+				batch_imgs = np.reshape(np.concatenate(batch[:, 0]), (-1,1000,1000))
+				batch_score = np.reshape(batch[:, 1], (-1))
+				train_step(batch_imgs, batch_score) 
+				current_step = tf.train.global_step(sess, global_step)
+				if current_step % FLAGS.evaluate_every == 0:
+					print("\nEvaluation:")
+					dev_step(test_score_imgs, test_score, writer=dev_summary_writer)
+					print("")
+				if current_step % FLAGS.checkpoint_every == 0:
+					path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+					print("Saved model checkpoint to {}\n".format(path))
+		else:
+			for batch in batches_label:
+				batch_imgs = np.reshape(np.concatenate(batch[:, 0]), (-1,1000,1000))
+				batch_score = np.reshape(batch[:, 1], (-1))
+				train_step(batch_imgs, batch_score) 
+				current_step = tf.train.global_step(sess, global_step)
+				if current_step % FLAGS.evaluate_every == 0:
+					print("\nEvaluation:")
+					dev_step(test_label_imgs, test_label, writer=dev_summary_writer)
+					print("")
+				if current_step % FLAGS.checkpoint_every == 0:
+					path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+					print("Saved model checkpoint to {}\n".format(path))
