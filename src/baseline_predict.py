ddd45fe8b3624bc92ca740d006848e0053712a60
Nini <gamelani@ethz.ch>
2018-07-03 17:33:25 +0200

    Same train and test sets as GAN model

--- src/baseline_predict.py
+++ src/baseline_predict.py
@@ -1,4 +1,3 @@
-from config import *
 import pandas as pd
 import numpy as np
 import os
@@ -6,69 +5,76 @@ from sklearn.externals import joblib
 import sklearn.metrics as skmet
 import PIL.Image
 
-from tweaked_cosmology import extract_feats
+from config import *
+from baseline_train import extract_feats
 
 
 # Parameters
 model = 'RandomForest' # Ridge or RandomForest
-feat_size = 50
+feat_size = 10
+train_ratio = 0.99
 y_data = ()
 
+folder = data_folder + 'generated_images'
+# folder = query_img_folder
 
 try:
-    query_mat = np.load(data_folder + 'query_mat_' + str(feat_size) + '.npy')
+    if folder == query_img_folder:
+        print("folder == query_img_folder")
+        query_df = pd.load_csv(data_folder + 'query_df_' + str(feat_size) + '.csv')
+    else:
+        query_df = pd.load_csv(data_folder + 'query_df_' + str(feat_size) + '.csv')
 
 except:
     # Preprocessing
-    print(len([name for name in os.listdir(query_img_folder)]))
-    num_img = len([name for name in os.listdir(query_img_folder)])
+    print(len([name for name in os.listdir(folder)]))
+    num_img = len([name for name in os.listdir(folder)])
     query_mat = np.zeros((num_img, feat_size))
 
     i = 0
-    index_mat = np.zeros(len([name for name in os.listdir(query_img_folder)]))
+    index_mat = []
 
-    for image in os.listdir(query_img_folder):
-        raw_image = PIL.Image.open(os.path.join(query_img_folder, image))
+    for image in os.listdir(folder):
+        raw_image = PIL.Image.open(os.path.join(folder, image))
         img_arr = np.array(raw_image.getdata()).reshape(raw_image.size[0], raw_image.size[1]).astype(np.uint8)
         img_feats = extract_feats(img_arr=img_arr, bins=feat_size)
-        # index_mat = np.append(index_mat, image.split(".")[0])
-
+        index_mat.append(image.split(".")[0])
         query_mat[i, :] = img_feats
+
         if i%100==0:
             print("Preprocessed {} images".format(i))
         i+=1
 
+    index_mat = np.asarray(index_mat)
+
+    # Creating dataframe
+    query_df = pd.DataFrame(data=query_mat, index=index_mat)
+
     print("Saving feature matrices...")
-    np.save(data_folder + 'query_mat_' + str(feat_size), query_mat)
-    # np.save(data_folder + 'query_mat_index_' + str(feat_size), query_mat)
+    if folder == query_img_folder:
+        query_df.to_csv(data_folder + 'query_df_' + str(feat_size) + '.csv')
 
-# Getting array with all image names
-index_mat = []
-for image in os.listdir(query_img_folder):
-    print(image.split(".")[0])
-    index_mat.append(image.split(".")[0])
-index_mat = np.asarray(index_mat)
-# np.save(data_folder + 'query_mat_index_' + str(feat_size), query_mat)
-
-query_mat = pd.DataFrame(data=query_mat, index=index_mat)
 
 # Loading model fitted model
-fitted_filename = cwd + '/fitted_' + str(model) + '_' + str(feat_size) + '.sav'
+fitted_filename = cwd + '/fitted_' + str(model) + '_' + str(train_ratio) + '_' + str(feat_size) + '.sav'
 ml_model = joblib.load(fitted_filename)
 
 # Predictions
 print("Predicting...")
-pred_data = ml_model.predict(query_mat)
+pred_data = ml_model.predict(query_df)
 
+# Saving Predictions
 try:
     prediction = pd.DataFrame({'Id': index_mat, 'Predicted': pred_data, 'True_value': y_data})
     print(prediction)
-    prediction.to_csv(str(model) + '_test_pred.csv')
-    print('Saved predictions')
     print("Mean absolute error (MAE): {}".format(skmet.mean_absolute_error(y_data, pred_data)))
 except:
-    print("No values given")
+    print("No true values given")
     prediction = pd.DataFrame(data=pred_data, columns=['Predicted'], index=index_mat)
-    prediction.to_csv(str(model) + '_' + str(feat_size) + '_query_pred.csv', index=True)
+    print(prediction)
+    if folder == query_img_folder:
+        prediction.to_csv(str(model) + '_' + str(feat_size) + '_query_pred.csv', index=True)
+    else:
+        prediction.to_csv(str(model) + '_' + str(feat_size) + '_trial_pred.csv', index=True)
     print('Saved predictions')
 
--- src/baseline_train.py
+++ src/baseline_train.py
@@ -1,10 +1,9 @@
 import os.path
-import random
 import csv
-
-import PIL.Image
 import numpy as np
 import pandas as pd
+import PIL.Image
+
 import sklearn.linear_model as sklm
 import sklearn.model_selection as skms
 import sklearn.ensemble as sken
@@ -36,7 +35,7 @@ def regressor(scorer, model='Ridge'):
 
 	elif model=='RandomForest':
 		base_model = sken.RandomForestRegressor(criterion="mae", max_features=None, oob_score=True,
-												random_state = 42)
+												random_state = 10)
 		ml_model = skms.GridSearchCV(base_model, {"n_estimators": [5, 10, 50, 100]},
 									 verbose=5,
 									 scoring=scorer)
@@ -45,87 +44,102 @@ def regressor(scorer, model='Ridge'):
 
 if __name__ == "__main__":
 
-	model = 'Ridge'
-	feat_size = 50
+	model = 'RandomForest'
+	feat_size = 10
+	train_ratio = 0.99
 
 	# Get preprocessed data if available
 	try:
-		train_mat = np.load(data_folder + 'train_mat_' + str(feat_size) + '.npy')
-		train_y = np.load(data_folder + 'train_y_' + str(feat_size) + '.npy')
-		test_mat = np.load(data_folder + 'test_mat_' + str(feat_size) + '.npy')
-		test_y = np.load(data_folder + 'test_y_' + str(feat_size) + '.npy')
+		train_mat = np.load(data_folder + 'train_mat_' + str(train_ratio) + '_' + str(feat_size) + '.npy')
+		train_y = np.load(data_folder + 'train_y_' + str(train_ratio) + '_' + str(feat_size) + '.npy')
+		test_mat = np.load(data_folder + 'test_mat_' + str(train_ratio) + '_' + str(feat_size) + '.npy')
+		test_y = np.load(data_folder + 'test_y_' + str(train_ratio) + '_' + str(feat_size) + '.npy')
 
 	# If preprocessed data doesn't exist, do it
 	except:
 		print("Couldn't find matrices; preprocessing data")
 
 		# Parameters
-		train_ratio = 0.7
 		print('feature_size: {}'.format(feat_size))
 
 		# Paths
 		scored_path = os.path.join(data_folder, "scored")
 		score_file = os.path.join(data_folder, "scored.csv")
 
-		# Initialization
-		score_dict = csv_to_dict(score_file) # Image number:(0 or 1)
-		img_prefixes = list(score_dict.keys()) # Image number (name)
-		random.shuffle(img_prefixes)
-		n_train = int(train_ratio * len(img_prefixes))
+		# Randomly shuffle data
+		np.random.seed(10)
 
+		# Initialization
+		score_dict = csv_to_dict(score_file)  # Creates a dictionary -> Image number: score (original order not kept)
+		score_df = pd.read_csv(score_file)  # Creates dataframe with Id and score (original order kept)
+		num_images = score_df.shape[0]
+		print("num images: {}".format(num_images))
+		shuffled_indices = list(np.random.permutation(num_images))
+		n_train = int(train_ratio * num_images)
+		n_test = num_images - n_train
 		print("n train: {}".format(n_train))
-		n_test = len(img_prefixes) - n_train
 		print("n test: {}".format(n_test))
+
 		train_mat = np.zeros((n_train, feat_size))
+		print("train mat shape: {}".format(train_mat.shape))
 		train_y = np.zeros(n_train)
 		test_mat = np.zeros((n_test, feat_size))
+		print("test mat shape: {}".format(test_mat.shape))
 		test_y = np.zeros(n_test)
 		train_idx = 0
 		test_idx = 0
 
 	###### FEATURE EXTRACTION ######
+
+		counter = 0
+
 		# Assemble train/test feature matrices / score vectors
-		for idx, img_prefix in enumerate(img_prefixes):
-			print("Image: {}/{}".format(idx + 1, len(img_prefixes)))
-			raw_image = PIL.Image.open(os.path.join(scored_path, "{}.png".format(img_prefix)))
+		for idx in shuffled_indices:
+			# print("idx: {}".format(idx))
+			if counter%500==0:
+				print("Image: {}/{}".format(counter, num_images))
+			img_index = int(score_df.iloc[idx]['Id'])
+			raw_image = PIL.Image.open(os.path.join(scored_path, "{}.png".format(img_index)))
 			img_arr = np.array(raw_image.getdata()).reshape(raw_image.size[0], raw_image.size[1]).astype(np.uint8)
 			img_feats = extract_feats(img_arr=img_arr, bins=feat_size)
-			score = float(score_dict[img_prefix])
+			score = float(score_dict[str(img_index)])
 
-			if idx < n_train:
-				train_mat[train_idx, :] = img_feats
-				train_y[train_idx] = score
-				train_idx += 1
-
-			else:
+			if (test_idx) < n_test:
 				test_mat[test_idx, :] = img_feats
 				test_y[test_idx] = score
 				test_idx += 1
 
+			else:
+				train_mat[train_idx, :] = img_feats
+				train_y[train_idx] = score
+				train_idx += 1
+
+			counter += 1
+
 		# Saving features/scores to disk
 		print("Saving feature matrices...")
-		np.save(data_folder + 'train_mat_' + str(feat_size) , train_mat)
-		np.save(data_folder + 'train_y_' + str(feat_size), train_y)
-		np.save(data_folder + 'test_mat_' + str(feat_size), test_mat)
-		np.save(data_folder + 'test_y_' + str(feat_size), test_y)
+		np.save(data_folder + 'train_mat_' + str(train_ratio) + '_' + str(feat_size) , train_mat)
+		np.save(data_folder + 'train_y_' + str(train_ratio) + '_' + str(feat_size), train_y)
+		np.save(data_folder + 'test_mat_' + str(train_ratio) + '_' + str(feat_size), test_mat)
+		np.save(data_folder + 'test_y_' + str(train_ratio) + '_' + str(feat_size), test_y)
 
 
-#### REGRESSION TRAINING ######
+#### REGRESSION TRAINING ####
 
 	ml_model = regressor(model=model, scorer='neg_mean_absolute_error')
 
 	print("Fitting...")
 	ml_model.fit(train_mat, train_y)
 
-	# print("Best params: {}".format(ml_model.cv_results_))
 	print("Best params: {}".format(ml_model.best_estimator_))
 
 # Saving model
-	fitted_filename = cwd + '/fitted_' + str(model) + '_' + str(feat_size) + '.sav'
+	fitted_filename = cwd + '/fitted_' + str(model) + '_' + str(train_ratio) + '_' + str(feat_size) + '.sav'
 	print(fitted_filename)
 	joblib.dump(ml_model, fitted_filename)
 
-##### PREDICTION ON TEST SET TAKEN FROM TRAINING SET #####
+
+#### PREDICTION ON TEST SET TAKEN FROM TRAINING SET ####
 	ml_model = joblib.load(fitted_filename)
 
 	# Predictions
@@ -134,7 +148,7 @@ if __name__ == "__main__":
 
 	prediction = pd.DataFrame({'predictions': pred_test_y, 'true_value': test_y})
 	print(prediction)
-	prediction.to_csv(str(model) + '_test_pred' + str(feat_size) + '.csv')
+	prediction.to_csv(str(model) + '_test_pred' + str(train_ratio) + '_' + str(feat_size) + '.csv')
 
 	# Classification diagnostics
 	print("Mean absolute error (MAE): {}".format(skmet.mean_absolute_error(test_y, pred_test_y)))
