c83022ec9e20d52d050ca23644ccc479c08c9bd0
melanibe <32590828+melanibe@users.noreply.github.com>
2018-07-04 17:02:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 global cwd
 
-""" Mélanie Bernhardt - Laura Manduchi - Mélanie Gaillochet.
+""" Melanie Bernhardt - Laura Manduchi - Melanie Gaillochet.
 
 File containing configuration variables used across the project.
 Do not change please.
